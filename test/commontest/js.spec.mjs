01548524baee211b43aacd8b54ee94a9b403968b
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-09-26 13:11:24 +0200

    feat: support body and request as functions

--- test/commontest/js.spec.mjs
+++ test/commontest/js.spec.mjs
@@ -66,6 +66,46 @@ describe("js mocks", function () {
         });
     });
 
+    test("body function (default)", async () => {
+        const res = await fetch(`http://${hostname}/api/js/body-fn`, {
+            method: "get",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const body = await res.json();
+        expect(body).to.deep.equal({
+            foo: "esm-body-fn",
+        });
+    });
+
+    test("body function (match)", async () => {
+        const res = await fetch(`http://${hostname}/api/js/body-fn`, {
+            method: "get",
+            headers: {
+                "Content-Type": "application/json",
+                "X-Foo": "1",
+            },
+        });
+        const body = await res.json();
+        expect(body).to.deep.equal({
+            foo: "bar",
+        });
+    });
+
+    test("reqeust function", async () => {
+        const res = await fetch(`http://${hostname}/api/js/request-fn`, {
+            method: "get",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const body = await res.json();
+        expect(body).to.deep.equal({
+            foo: "bar",
+        });
+    });
+
     // Invalid format, which we still need to support
     test("should support invalid format where mock returns a string instead of object", async () => {
         const res = await fetch(`http://${hostname}/api/js/invalid-stringify`);
